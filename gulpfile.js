d3397ef203642f7bef76aa70b16266391f8a3df4
Richard McCartney <richard@mccartney.io>
2017-03-05 19:19:27 +0000

    Spelling amendments

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,69 @@
-var gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    notify = require('gulp-notify'),
-    gutil = require('gulp-util'),
-    scsslint = require('gulp-scss-lint'),
-    cssnano = require('gulp-cssnano');
-    util = require('util');
+var
+gulp = require('gulp'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer'),
+cssnano = require('gulp-cssnano'),
+sassLint = require('gulp-sass-lint'),
+rename = require('gulp-rename'),
+sourcemaps = require('gulp-sourcemaps'),
+sassPath = 'scss/maas-gui-framework.build-1.2.4.scss';
 
-/* Helper functions */
+// Gulp help instructions triggered as Gulp default task
+gulp.task('help', function() {
+  console.log('develop - Watch sass files and generate unminified CSS for development');
+  console.log('test  - Lint Sass');
+  console.log('build  - Lint Sass files and generate minified CSS for production');
+});
+
+// Provide details of Sass errors
 function throwSassError(sassError) {
-    throw new gutil.PluginError({
-        plugin: 'sass',
-        message: util.format(
-            "Sass error: '%s' on line %s of %s",
-            sassError.message,
-            sassError.line,
-            sassError.file
-        )
-    });
+  throw new gutil.PluginError({
+    plugin: 'sass',
+    message: util.format(
+      "Sass error: '%s' on line %s of %s",
+      sassError.message,
+      sassError.line,
+      sassError.file
+    )
+  });
 }
 
-/* Gulp instructions start here */
-gulp.task('help', function() {
-    console.log('sass - Generate the min and unminified css from sass');
-    console.log('build - Generate css');
-    console.log('watch - Watch sass files and generate unminified css');
-    console.log('test - Lints Sass');
+// Lints Sass and provides errors
+gulp.task('lint:sass', function() {
+  return gulp.src('scss/**/*.s+(a|c)ss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
 });
 
-gulp.task('sasslint', function() {
-    return gulp.src('scss/**/*.scss')
-        .pipe(scsslint({ 'config': '.scss-lint.yml' }))
-        .pipe(scsslint.failReporter());
+// Build Sass for local development
+gulp.task('sass:develop', function() {
+  return gulp.src(sassPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ style: 'expanded', errLogToConsole: true }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(sourcemaps.write('maps/'))
+    .pipe(gulp.dest('css/'))
 });
 
-gulp.task('sass', function() {
-    return gulp.src('scss/**/*.scss')
-        .pipe(sass({
-            style: 'expanded',
-            errLogToConsole: true,
-            onError: throwSassError
-        }))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(gulp.dest('css/'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cssnano())
-        .pipe(gulp.dest('css/'));
+// Build Sass for production
+gulp.task('sass:build', function() {
+  return gulp.src(sassPath)
+    .pipe(sass({
+      style: 'compressed',
+      errLogToConsole: true,
+      omitSourceMapUrl: true,
+      onError: throwSassError
+    }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest('css/'))
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css/'));
 });
 
-gulp.task('sass-lite', function() {
-    return gulp.src('scss/maas-gui-framework.build-1.2.3.scss')
-        .pipe(sass({ style: 'expanded', errLogToConsole: true }))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(gulp.dest('css/'));
-});
-
-gulp.task('watch', function() {
-    gulp.watch('scss/**/*.scss', ['sass-lite']);
-});
-
+// Gulp default tasks
 gulp.task('default', ['help']);
-gulp.task('test', ['sasslint']);
-gulp.task('build', ['sasslint', 'sass']);
+gulp.task('develop', ['watch', 'sass:develop']);
+gulp.task('test', ['lint:sass']);
+gulp.task('build', ['lint:sass', 'sass:build']);
